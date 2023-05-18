aee5d564bff7f79b992665ded1615768716ccc7f
Dakota <dmayhem93@gmail.com>
2023-05-18 11:32:40 -0500

    ran black

--- data_handlers/embedding_clustering.py
+++ data_handlers/embedding_clustering.py
@@ -3,11 +3,11 @@ import numpy as np
 from sklearn.cluster import KMeans
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     dataset = load_dataset("dmayhem93/self-critiquing-base-topic-embeddings")
     np_data = list()
-    for i in range(len(dataset['train'])):
-        np_data.append(np.array(dataset['train'][i]['embedding']))
+    for i in range(len(dataset["train"])):
+        np_data.append(np.array(dataset["train"][i]["embedding"]))
     np_data = np.vstack(np_data)
     kmeans = KMeans(n_clusters=900, init="k-means++", random_state=42)
     kmeans.fit(np_data)
@@ -16,5 +16,5 @@ if __name__ == '__main__':
     out_list = [None for i in range(900)]
     for i in range(len(labels)):
         if out_list[int(labels[i])] is None:
-            out_list[int(labels[i])] = dataset['train'][i]
+            out_list[int(labels[i])] = dataset["train"][i]
     Dataset.from_list(out_list).push_to_hub("self-critiquing-base-selected-900")
--- data_handlers/get_embeddings.py
+++ data_handlers/get_embeddings.py
@@ -12,34 +12,36 @@ def get_embedding(text: str, model="text-embedding-ada-002") -> list[float]:
     return openai.Embedding.create(input=[text], model=model)["data"][0]["embedding"]
 
 
-
 def get_unique_test_data(dataset):
     out_data = dict()
-    for item in tqdm.tqdm(dataset['test']):
+    for item in tqdm.tqdm(dataset["test"]):
         # only use topic summarization tasks
-        if not item['is_topic_based_summarization']:
+        if not item["is_topic_based_summarization"]:
             continue
-        if item['id'] not in out_data.keys():
-            out_data[item['id']] = {
-                "id": item['id'],
-                "split": item['split'],
-                "time": item['time'],
-                "labeler": item['labeler'],
-                "is_topic_based_summarization": item['is_topic_based_summarization'],
-                'prompt': item['prompt'],
-                'responses': [item['response']]
+        if item["id"] not in out_data.keys():
+            out_data[item["id"]] = {
+                "id": item["id"],
+                "split": item["split"],
+                "time": item["time"],
+                "labeler": item["labeler"],
+                "is_topic_based_summarization": item["is_topic_based_summarization"],
+                "prompt": item["prompt"],
+                "responses": [item["response"]],
             }
         else:
-            out_data[item['id']]['responses'].append(item['response'])
+            out_data[item["id"]]["responses"].append(item["response"])
     return list(out_data.values())
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     dataset = load_dataset("dmayhem93/self-critiquing-base")
     to_be_embedded = get_unique_test_data(dataset)
     print(to_be_embedded[0])
     for i in tqdm.tqdm(range(len(to_be_embedded))):
-        to_be_embedded[i]['embedding'] = get_embedding(to_be_embedded[i]['prompt'], model="text-embedding-ada-002")
-    Dataset.from_list(to_be_embedded).push_to_hub("self-critiquing-base-topic-embeddings")
+        to_be_embedded[i]["embedding"] = get_embedding(
+            to_be_embedded[i]["prompt"], model="text-embedding-ada-002"
+        )
+    Dataset.from_list(to_be_embedded).push_to_hub(
+        "self-critiquing-base-topic-embeddings"
+    )
 # print(len(embedding))
