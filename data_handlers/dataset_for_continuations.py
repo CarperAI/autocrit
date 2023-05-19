6a57f4b7c56684686f7a465c1d11e5970f315671
Dakota <dmayhem93@gmail.com>
2023-05-19 09:07:31 -0500

    added prompt continuation script

---
+++ data_handlers/dataset_for_continuations.py
@@ -0,0 +1,21 @@
+from datasets import Dataset, load_dataset
+import numpy as np
+from sklearn.cluster import KMeans
+
+
+if __name__ == "__main__":
+    dataset = load_dataset("dmayhem93/self-critiquing-base-selected-900")
+    out_list = list()
+    for item in dataset['train']:
+        out_list.append({
+            "id": item["id"],
+            "split": item["split"],
+            "time": item["time"],
+            "labeler": item["labeler"],
+            "is_topic_based_summarization": item["is_topic_based_summarization"],
+            "prompt": item["prompt"] + item['responses'][0].split("Answer:")[0] + "Answer:",
+        })
+    # for i in range(len(labels)):
+    #     if out_list[int(labels[i])] is None:
+    #         out_list[int(labels[i])] = dataset["train"][i]
+    Dataset.from_list(out_list).push_to_hub("self-critiquing-base-selected-900-prompt-continuation")
