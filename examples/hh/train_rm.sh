0bcdbd17322c238ebae549b9d46dc8e935eded91
reciprocated <56548574+reciprocated@users.noreply.github.com>
2023-06-16 00:51:55 +0300

    chore: drop code from reward-modeling

--- examples/hh/train_rm.sh
+++
@@ -1,3 +0,0 @@
-deepspeed --num_gpus=8 ../../tclx/train/finetune_scalar_rm.py --config_path rm_config.yaml \
---ds_config_path ../../configs/ds_configs/ds_config_gpt_j.json \
---deepspeed ../../configs/ds_configs/ds_config_gpt_j.json
--- examples/hh/train_sft.sh
+++
@@ -1,3 +0,0 @@
-deepspeed --num_gpus=8 ../../tclx/train/finetune_base.py --config_path sft_config.yaml \
---ds_config_path ../../configs/ds_configs/ds_config_gpt_j.json \
---deepspeed ../../configs/ds_configs/ds_config_gpt_j.json
