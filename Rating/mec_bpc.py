8042577001631bbba4ab3b35ce81c01101feefa3
Dakota <dmayhem93@gmail.com>
2023-09-14 12:03:38 -0500

    fix and update mec_bpc.py to actually add mirrored response

--- Rating/mec_bpc.py
+++ Rating/mec_bpc.py
@@ -1,36 +1,13 @@
-import guidance
-import jsonlines
 import json
+from time import sleep
+from tqdm import tqdm
+import openai
+from string import Template
+from typing import Optional, List, Union
 
-# Vicuna & Chat GPT Data from https://github.com/i-Eval/FairEval/tree/main/answer
-# Questions from https://github.com/lm-sys/FastChat/blob/main/fastchat/eval/table/question.jsonl
-# System Prompts from https://github.com/lm-sys/FastChat/blob/main/fastchat/eval/table/prompt.jsonl
-# Human Eval form https://github.com/i-Eval/FairEval/blob/main/review/review_gpt35_vicuna-13b_human.txt
 
-
-def load_data(vicuna, gpt35, question, prompts):
-    with jsonlines.open(vicuna) as reader:
-        vicuna_data = list(reader.iter())
-    with jsonlines.open(gpt35) as reader:
-        gpt35_data = list(reader.iter())
-    with jsonlines.open(question) as reader:
-        question_data = list(reader.iter())
-    with jsonlines.open(prompts) as reader:
-        prompts_data = list(reader.iter())
-    output_data = list()
-    prompts = {prompt_data['category']: prompt_data for prompt_data in prompts_data}
-    for i in range(len(vicuna_data)):
-        if vicuna_data[i]["question_id"] != question_data[i]["question_id"]:
-            raise ValueError("Question mismatch")
-        if gpt35_data[i]["question_id"] != question_data[i]["question_id"]:
-            raise ValueError("Question mismatch")
-        output_data.append([
-            question_data[i]['text'],
-            vicuna_data[i]['text'],
-            gpt35_data[i]['text'],
-            prompts[question_data[i]['category']] if question_data[i]['category'] in list(prompts.keys()) else prompts['general']
-        ])
-    return output_data
+MAX_API_RETRY = 5
+REQ_TIME_GAP = 5
 
 
 def clean_continuation(rating):
@@ -55,32 +32,44 @@ def extract_rating(rating):
         return None
 
 
+def get_eval(system_prompt, user_prompt: str):
+    ratings = [None for _ in range(3)]
+    for rating_idx in range(3):
+        retry_count = 0
+        for _ in range(MAX_API_RETRY):
+            try:
+                messages = [{"role": "system", "content": system_prompt}] if system_prompt else []
+                messages.append({"role": "user", "content": user_prompt})
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=messages,
+                    temperature=0.0,
+                    max_tokens=512,
+                )
+
+                ratings[rating_idx] = response["choices"][0]["message"]["content"]
+                break
+            except Exception as e:
+                print(e)
+                sleep(REQ_TIME_GAP)
+                retry_count += 1
+        if retry_count == MAX_API_RETRY:
+            raise Exception(f"Failed after {MAX_API_RETRY} retries.")
+    score = [extract_rating(rating) for rating in ratings]
+    score = [s for s in score if s is not None]
+    if len(score) == 0:
+        return ratings, None
+    else:
+        return ratings, sum(score) / len(score)
 
 
-def mec_bpc(question, vicuna, gpt35, prompt_detail, num_evaluations=3):
-    """
-    Performs a MEC-BPC evaluation on a question, vicuna, and gpt35 continuation.
-    The return value is a list of the following format:
-    [question, vicuna, gpt35, prompt_detail['system_prompt'], [answer], [mirrored_answer]]
-    mirrored_answer should be multiplied by -1 and added to answer to get the final score.
-    example at the bottom to see how to use for evaluation.
-
-    :param question: Prompt used to generate a continuation
-    :param vicuna: Vicuna continuation
-    :param gpt35: ChatGPT continuation
-    :param prompt_detail: prompt details
-    :param num_evaluations: Number of evaluations to perform with guidance
-    :return: [question, vicuna, gpt35, prompt_detail['system_prompt'], [answer], [mirrored_answer]]
-    """
-    system_text = "{{#system~}}\n" + prompt_detail['system_prompt'] + "\n{{~/system}}\n"
-    program = guidance(system_text + """{{#user~}}
-[Question]
-{{Q}}
+mecbpc_template = Template("""[Question]
+$q
 [The Start of Assistant 1’s Answer]
-{{R1}}
+$a1
 [The End of Assistant 1’s Answer]
 [The Start of Assistant 2’s Answer]
-{{R2}}
+$a2
 [The End of Assistant 2’s Answer]
 [System]
 We would like to request your feedback on the performance of two AI assistants in response to the user question displayed above.
@@ -91,60 +80,44 @@ Then, output two lines indicating the scores for Assistant 1 and 2, respectively
 Output with the following format:
 Evaluation evidence: <evaluation explanation here>
 The score of Assistant 1: <score>
-The score of Assistant 2: <score>
-{{~/user}}
-{{#assistant~}}
-{{gen 'evaluation' temperature=1 max_tokens=512}}
-{{~/assistant}}
-""", caching=False)
-    answer = [extract_rating(program(Q=question, R1=vicuna, R2=gpt35)['evaluation']) for i in range(num_evaluations)]
-    mirrored_answer = [extract_rating(program(Q=question, R1=gpt35, R2=vicuna)['evaluation']) for i in range(num_evaluations)]
-    return {"question": question,
-            "vicuna": vicuna,
-            "gpt35": gpt35,
-            "prompt": prompt_detail['system_prompt'],
-            "answer": answer,
-            "mirrored_answer": mirrored_answer}
+The score of Assistant 2: <score>""")
+
+
+def mecpbc_score(systems: Optional[List[str]], prompts: List[str], outputs: List[List[str]]) -> List[float]:
+    if isinstance(prompts, str) and isinstance(outputs, str):
+        prompts = [prompts]
+        outputs = [outputs]
+    if systems is not None:
+        assert len(systems) == len(prompts) == len(outputs), "Number of systems, prompts and outputs must be equal"
+    else:
+        assert len(prompts) == len(outputs), "Number of prompts and outputs must be equal"
+        systems = [""] * len(prompts)
+    assert all(len(output) == 2 for output in outputs), "Must have two responses to rate"
+
+    data = []
+    for system, prompt, output in tqdm(zip(systems, prompts, outputs)):
+        rating = get_eval(system, mecbpc_template.substitute(q=prompt, a1=output[0], a2=output[1]))
+        mirror_rating = get_eval(system, mecbpc_template.substitute(q=prompt, a1=output[1], a2=output[0]))
+        if rating[1] is None or mirror_rating[1] is None:
+            score = None
+        else:
+            score = (rating[-1] + mirror_rating[-1]) / 2
+        data.append({"prompt": prompt,
+                     "output": output,
+                     "critique": rating[0],
+                     "mirrored_critique": mirror_rating[0],
+                     "score": score})
+        with open("mec_pbc.json", "w") as f:
+            json.dump(data, f, indent=4)
+
+    return [d["score"] for d in data]
 
 
 if __name__ == '__main__':
-    guidance.llm = guidance.llms.OpenAI("gpt-3.5-turbo")
-    data = load_data(
-        "answer_vicuna-13b.jsonl",
-        "answer_gpt35.jsonl",
-        "question.jsonl",
-        "prompt.jsonl"
+    scores = mecpbc_score(
+        None,
+        prompts=["What is the capital of France?"] * 3,
+        outputs=[["Paris", "Paris, but I'm not sure"],
+                 ["idk", "What's France?"],
+                 ["In english or french?", "Paris"]],
     )
-    outputs = list()
-    for i in range(len(data)):
-        outputs.append(mec_bpc(data[i][0], data[i][1], data[i][2], data[i][3]))
-        with open("mec_bpc.json", "w") as f:
-            json.dump(outputs, f, indent=4)
-    with open("review_gpt35_vicuna-13b_human.txt") as f:
-        human = f.readlines()
-    print(human)
-    matches = 0
-    total = 0
-    for i, item in enumerate(outputs):
-        scores = [score for score in item['answer'] if score is not None]
-        mirrored_scores = [score for score in item['mirrored_answer'] if score is not None]
-        correct = False
-        if len(scores) == 0:
-            print("No score for question {}".format(i))
-        elif len(mirrored_scores) == 0:
-            print("No mirrored score for question {}".format(i))
-        else:
-            tot_score = (sum(scores) / len(scores)) - (sum(mirrored_scores) / len(mirrored_scores))
-            if tot_score < 0:
-                if 'CHATGPT' in human[i]:
-                    correct = True
-            elif tot_score == 0:
-                if 'TIE' in human[i]:
-                    correct = True
-            else:
-                if 'VICUNA' in human[i]:
-                    correct = True
-            if correct:
-                matches += 1
-            total += 1
-    print("Accuracy: {}, Matches: {}, Total: {}".format(matches/total, matches, total))
